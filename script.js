f4938766e4a1cc4ccbe35d36489eb2fcaf5175fd
SetonRoboticsMFoor <48261500+SetonRoboticsMFoor@users.noreply.github.com>
2022-05-19 13:17:40 -0700

    Final working code

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const cards = ["Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","Ki
 const suits = ["Hearts","Diamonds","Clubs","Spades"];
 const deck = [];
 
-for(let i in cards) {
-    deck.push(cards[i]);
+for(let i in suits) {
+    for(let r in cards) {
+
+        deck.push(cards[r] + suits[i]);
+    }
+
 }
 
 alert(deck);
